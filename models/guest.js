3222fe8f5e5ee63945723c46a63654e4b9bd482d
naokiono <kyomini@qq.com>
2018-08-03 07:28:24 +0800

    guest 留言板模型

--- models/guest.js
+++ models/guest.js
@@ -3,14 +3,9 @@ var Schema = require('mongoose').Schema;
 
 var guestSchema = new Schema({
     name: String,
-    password: String,
-    icon: String,
     sex: String,
-    tel: String,
-    qq: String,
-    weixin: String,
-    weibo: String,
-    email: String
+    email: String,
+    content: String,
 });
 
 module.exports = guestSchema;

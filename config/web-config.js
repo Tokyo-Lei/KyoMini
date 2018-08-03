e7c56fbf0b00569ced3b46880fca88d0d4b72419
naokiono <kyomini@qq.com>
2018-08-03 11:00:13 +0800

    新增配置参数

--- config/web-config.js
+++ config/web-config.js
@@ -1,8 +1,10 @@
 
 module.exports = {
 
+    template:'template',
+    extname:'.html',
     db:mongoose.connect('mongodb://127.0.0.1/blog'),
-    port = process.env.PORT || 3000
-
+    port = 'process.env.PORT || 3000'.
+    debug = 'process.env.NODE_ENV !== "production"';
 
 };

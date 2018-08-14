731a82446dfac41ed90e205500ee16cb31e107b7
naokiono <kyomini@qq.com>
2018-08-14 15:02:21 +0800

    更新

--- app/app.js
+++ app/app.js
@@ -7,12 +7,11 @@
  *  ╚╝╚═╝─╚╝─╚═══╩╝╚╝╚╩══╩╝─╚═╩══╝
  *  Node.js For KyoMini框架
  *  Author:小野直树
- *
+ *  Version:1.0.0
  */
 
 
 const Koa = require('koa'),
-			Router = require('koa-router'),
 			logger = require('koa-logger'),
 			serve = require('koa-static'),
 			path = require('path'),
@@ -27,33 +26,27 @@ const Koa = require('koa'),
 
 
 
+
+
+
+
+
+
     //加载helmet安全模块 防止XSS攻击等
     app.use(helmet());
-   
-
 	//加载路由
 	routers(app);
-
-  
 	//静态资源处理的中间件
-	app.use(serve(path.resolve(__dirname,'./public/')));	
-	
-
-
+	app.use(serve(path.resolve(__dirname,'./public/')));
 	render(app, {
 				root: path.join(__dirname, config.template), //模板所在文件夹目录
 				extname: config.extname,   //模板文件后缀名
 				debug: config.debug //调试模式
 	 });
-
-	 
 	//获取POST/GET中间件
     app.use(bodyParser());
-	 
-	 
 	//日志输出插件
 	app.use(logger());
-
 	app.listen(config.port,function(){
 		console.log(config.web_info);
 	});

56cdc5d28d9d162231ea352ce04b7b5ba425a8e2
naokiono <kyomini@qq.com>
2018-08-15 14:34:21 +0800

    修正错误

--- app/config/web-config.js
+++ app/config/web-config.js
@@ -6,9 +6,9 @@ module.exports = {
     debug : process.env.NODE_ENV !== 'production',
     web_info : '╔╗╔═╦╗──╔╦═══╦═╗╔═╦══╦═╗─╔╦══╗\n║║║╔╣╚╗╔╝║╔═╗║║╚╝║╠╣╠╣║╚╗║╠╣╠╝\n║╚╝╝╚╗╚╝╔╣║─║║╔╗╔╗║║║║╔╗╚╝║║║\n║╔╗║─╚╗╔╝║║─║║║║║║║║║║║╚╗║║║║\n║║║╚╗─║║─║╚═╝║║║║║╠╣╠╣║─║║╠╣╠╗\n╚╝╚═╝─╚╝─╚═══╩╝╚╝╚╩══╩╝─╚═╩══╝\n\n'
     +'欢迎使用KyoMini For Node.js框架 V0.0.2版\n\n'
-    +'√ npm install  依赖安装完毕！\n'
-    +'√ npm start    服务器启动成功！\n'
-    +'√ 请打开地址：http://localhost:8080',
-    mongodb_success :'√ mongoose数据库连接成功！',
-    mongodb_error :'× mongoose数据库连接失败,原因:'
+    +'[√] npm install  依赖安装完毕！\n'
+    +'[√] npm start    服务器启动成功！\n'
+    +'[√] 请打开地址：http://localhost:8080',
+    mongodb_success :'[√] mongoose数据库连接成功！',
+    mongodb_error :'[×] mongoose数据库连接失败,原因:'
 };

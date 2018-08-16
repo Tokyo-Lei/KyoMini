1f5f4043cf7edf6b55f303e4fcf23e12a2183835
Naoki Ono <krosash@gmail.com>
2018-08-16 10:31:35 +0800

    新增数据库模块

---
+++ app/models/mongoodb.js
@@ -0,0 +1,24 @@
+/*
+* @ use 数据库连接
+*/
+const mongoose = require('mongoose');
+const config = require('../../config/common');
+
+const dbConfig = config[process.env.NODE_ENV||'development'];
+
+mongoose.connect(dbConfig.mongo.uri);
+
+// 连接成功
+mongoose.connection.on('connected', function() {
+  console.log('Mongoose connection open to ' + dbConfig.mongo.uri);
+});
+
+// 连接失败
+mongoose.connection.on('error', function(err) {
+  console.log('Mongoose connection error: ' + err);
+});
+
+// 断开连接
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose connection disconnected');
+});
